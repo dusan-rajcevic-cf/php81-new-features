1edfccbc08f2abae43cdba84523372c4f0f1e5e5
Dusan Rajcevic <dusan@awardforce.com>
2024-03-01 09:12:31 +0100

    Added instance method 'printValue' to the UserRole enum.

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -7,4 +7,13 @@ enum UserRole: string
     case ADMIN = 'admin';
     case MANAGER = 'manager';
     case VISITOR = 'visitor';
+
+    public function printValue(): string
+    {
+        return match ($this) {
+            self::ADMIN => 'Administrator',
+            self::MANAGER => 'Manager',
+            self::VISITOR => 'Visitor',
+        };
+    }
 }
